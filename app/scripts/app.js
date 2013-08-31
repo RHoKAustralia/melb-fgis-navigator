2177ad58313e4420ba0ec8ac11272e217323a2b8
Mark Wolfe <mark@wolfe.id.au>
2013-08-31 14:57:59 +1000

    Map loads needs some styling and structure.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('navigatorApp', [])
+angular.module('navigatorApp', ['leaflet-directive'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,11 +1,15 @@
 'use strict';
 
-angular.module('navigatorApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'Less',
-      'AngularJS',
-      'Karma'
-    ];
+angular.module('navigatorApp').controller('MainCtrl', function ($scope) {
+
+  console.log('$scope', 'MainCtrl');
+
+  angular.extend($scope, {
+    london: {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 4
+    }
   });
+
+});
